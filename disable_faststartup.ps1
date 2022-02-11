750fa9355221665281138bc6f8039bf6faa0cff9
Antonio Raquel <antonioraquel@outlook.com>
2022-02-11 11:25:48 -0500

    Add files via upload
    Disable Fast Startup through Intune

---
+++ disable_faststartup.ps1
@@ -0,0 +1,6 @@
+﻿try {
+	if(-NOT (Test-Path -LiteralPath "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power")){ return $false };
+	if((Get-ItemPropertyValue -LiteralPath 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power' -Name 'HiberbootEnabled' -ea SilentlyContinue) -eq 0) {  } else { return $false };
+}
+catch { return $false }
+return $true
